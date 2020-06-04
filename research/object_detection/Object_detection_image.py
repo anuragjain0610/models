40d744dd1a80ba510d0c27e6325c974a5d312108
Anurag Jain <anuragjain0610@gmail.com>
2020-06-05 02:02:31 +0530

    Update Object_detection_image.py

--- research/object_detection/Object_detection_image.py
+++ research/object_detection/Object_detection_image.py
@@ -47,7 +47,7 @@ PATH_TO_LABELS = os.path.join(CWD_PATH,'training','labelmap.pbtxt')
 PATH_TO_IMAGE = os.path.join(CWD_PATH,IMAGE_NAME)
 
 # Number of classes the object detector can identify
-NUM_CLASSES = 6
+NUM_CLASSES = 3
 
 # Load the label map.
 # Label maps map indices to category names, so that when our convolution
@@ -111,10 +111,10 @@ vis_util.visualize_boxes_and_labels_on_image_array(
     min_score_thresh=0.60)
 
 # All the results have been drawn on image. Now display the image.
-cv2.imshow('Object detector', image)
+# cv2.imshow('Object detector', image)
+cv2.imwrite('result.jpg', image)
+# # Press any key to close the image
+# cv2.waitKey(0)
 
-# Press any key to close the image
-cv2.waitKey(0)
-
-# Clean up
-cv2.destroyAllWindows()
+# # Clean up
+# cv2.destroyAllWindows()
